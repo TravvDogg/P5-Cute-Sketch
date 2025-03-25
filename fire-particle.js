4118c4551573d2ff43be99bd2c24b2f6eaa24c79
Travis Lizio <liziotravis@gmail.com>
2025-03-25 19:20:12 +1100

    Begin Commit - better late than never

---
+++ fire-particle.js
@@ -0,0 +1,84 @@
+class FireParticle {
+    // Define the parameters for each Fire Particle
+    constructor(x, y) {
+
+    // Define starting parameters
+        
+        // Define particle starting coordinates
+        this.x = x
+        this.y = y
+        
+        // Give the fire random velocity, to move up and to the side at varying speeds
+        this.vx = random(-0.4, 0.4)
+        this.vy = random(-4, -0.5)
+
+        // Start the particle as transparent, to fade in
+        this.alpha = 0
+        this.fadingIn = true
+
+        // Vary the starting sizes of each particle
+        this.size = random(20, 28)
+
+        // Make sure we use the correct color mode
+        colorMode(RGB)
+        // Random color at differing shades of red / orange / yellow
+        this.color = [random(230, 255), random(140, 180), random(50, 80)]
+
+    // Reduce the particle's 'visual energy' as they progress 
+    // higher values will damp less
+
+        // Reduce the speed they move sideways and up
+        this.damping_x = 0.95
+        this.damping_y = 0.975
+
+        // Reduce the size and opacity of each particle
+        this.decay_size = 0.98
+        this.decay_alpha = 0.97
+
+        // Random value for the particles to sway back and forth with the 'wind'
+        this.sway = random(10)
+    }
+
+    update() {
+        // Translate each particle by their velocity
+        // And apply sway to the particles over time. 
+        // Increase coefficient of sin() for more spread
+        this.x += this.vx + (sin(this.sway) * 0.2)
+
+        this.y += this.vy
+
+        // Reduce velocity over time
+        this.vx *= this.damping_x 
+        this.vy *= this.damping_y
+        
+        // Reduce size over time
+        this.size *= this.decay_size
+        
+        // Advance 'sway' over time
+        // Larger values will increase their oscillating left-to-right energy. 
+        this.sway += 0.02
+        
+        // Fade particles in then out
+        if (this.alpha < 255 && this.fadingIn) {
+             //Fade in over 8 frames
+            this.alpha += 255 / 8
+        } else {
+            // Once faded in, begin fading each particle out
+            this.fadingIn = false
+            this.alpha *= this.decay_alpha
+        }
+    }
+
+    show() {
+        colorMode(RGB)
+        noStroke()
+        // Use a softer, more varied fire color
+        fill(this.color[0], this.color[1], this.color[2], this.alpha)
+        ellipse(this.x, this.y, this.size)
+
+    }
+
+    isFinished() {
+        return this.alpha <= 1
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,86 @@
+// Header
+
+// Marshmallow burning Sound effect by ZapSplat (ZapSplat Standard Lisence)
+// Campfire and forest sounds by West Wolf (ZapSplat Standard Lisence)
+
+// ZapSplat Standard Lisence does not require links to each sound effect
+// https://www.zapsplat.com/license-type/standard-license/
+
+
+
+
+let SFX_marshmallow_burn
+let SFX_campfire
+
+
+function preload() {
+  
+}
+
+function setup() {
+  createCanvas(400, 400)
+  frameRate(60)
+}
+
+
+// Initialise 
+let fire_particles = []
+// Initialise coordinates of fire base
+let fire_base_x
+let fire_base_y
+
+let fire_lit = false
+function draw() {
+  // ---------------------------------Execute Regardless of Fire State-----------------------------
+  colorMode(RGB)
+  background(50)
+
+  // Draw fire from the middle of the screen
+  fire_base_x = width / 2
+  // Draw fire 3/4 the height of the screen
+  fire_base_y = 3 * height / 4
+
+
+  // Update and display all fire_particles
+  for (let i = fire_particles.length - 1; i >= 0; i--) {
+    fire_particles[i].update()
+    fire_particles[i].show()
+
+    // Remove fire_particles when they have faded out
+    if (fire_particles[i].isFinished()) {
+      fire_particles.splice(i, 1)
+    }
+  }
+
+
+  // Logic for the beginning of the simulation, before the fire is lit
+  if (!fire_lit) {
+  // ---------------------------------Execute While Fire is Unlit----------------------------------
+
+    // Draw the 'open eyes' or 'light campfire' text, on black screen
+
+
+
+
+
+
+    return
+  }
+  // ---------------------------------Execute While Fire is Lit------------------------------------
+
+  // Add a flame particle to the base of the fire on varying frames
+  if (frameCount % random(1, 2) <= 1) {
+    // Draw a flame particle at a random place along the width of the base of the fire
+    fire_particles.push(new FireParticle(fire_base_x + random(-20, 20), fire_base_y))
+  }
+
+
+}
+
+// Toggle the fire when mouse is clicked
+function mouseClicked() {
+  // if (!fire_lit) {
+  //   fire_lit = true
+  // }
+  fire_lit = !fire_lit
+}
