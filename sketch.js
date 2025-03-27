846419ed9536ba5a0cf7503cf4124944aa3f2e8b
Travis Lizio <liziotravis@gmail.com>
2025-03-27 11:44:33 +1100

    Create smoke-stream class

--- sketch.js
+++ sketch.js
@@ -17,42 +17,50 @@ function preload() {
   
 }
 
-function setup() {
-  createCanvas(400, 400)
-  frameRate(60)
-}
-
-
-// Initialise 
+// Initialise fire_particles array
 let fire_particles = []
 // Initialise coordinates of fire base
 let fire_base_x
 let fire_base_y
 
-let fire_lit = false
-function draw() {
-  // ---------------------------------Execute Regardless of Fire State-----------------------------
-  colorMode(RGB)
-  background(50)
+// Initialise smoke_stream
+let smoke_stream
+
+function setup() {
+  createCanvas(400, 400)
+  frameRate(60)
 
   // Draw fire from the middle of the screen
   fire_base_x = width / 2
   // Draw fire 3/4 the height of the screen
   fire_base_y = 3 * height / 4
 
+  // Assign the class SmokeStream to the variable smoke_stream
+  smoke_stream = new SmokeStream(fire_base_x + random(-10, 10), fire_base_y)
+}
 
+let fire_lit = true
+function draw() {
+  // ---------------------------------Execute Regardless of Fire State-----------------------------
+  colorMode(RGB)
+  background(50)
+
+  
   // Update and display all fire_particles
   for (let i = fire_particles.length - 1; i >= 0; i--) {
     fire_particles[i].update()
     fire_particles[i].show()
-
+    
     // Remove fire_particles when they have faded out
     if (fire_particles[i].isFinished()) {
       fire_particles.splice(i, 1)
     }
   }
-
-
+  
+  // Update and display smoke_stream
+  smoke_stream.update()
+  smoke_stream.show()
+  
   // Logic for the beginning of the simulation, before the fire is lit
   if (!fire_lit) {
   // ---------------------------------Execute While Fire is Unlit----------------------------------
@@ -73,8 +81,6 @@ function draw() {
     // Draw a flame particle at a random place along the width of the base of the fire
     fire_particles.push(new FireParticle(fire_base_x + random(-20, 20), fire_base_y))
   }
-
-
 }
 
 // Toggle the fire when mouse is clicked
